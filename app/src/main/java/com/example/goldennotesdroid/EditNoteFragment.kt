0dcc892d977929ce9b5984218ce6ea64283f58d7
Omkar Shirgaonkar <shirgaonkarom@yahoo.in>
2023-10-24 13:10:55 +0530

    Add navigation from NotesList page to EditNote page
    This commit adds navigation support from the parent NotesList page to
    the EditNote page for editing a single note.

--- app/src/main/java/com/example/goldennotesdroid/EditNoteFragment.kt
+++ app/src/main/java/com/example/goldennotesdroid/EditNoteFragment.kt
@@ -5,55 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.goldennotesdroid.databinding.FragmentEditNoteBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EditNoteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditNoteFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var titleText: String = ""
+    private var bodyText: String = ""
+
+    private var _binding: FragmentEditNoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            titleText = it.getString(NOTE_TITLE).toString()
+            bodyText = it.getString(NOTE_BODY).toString()
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentEditNoteBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editTitle.setText(titleText)
+        binding.editBody.setText(bodyText)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditNoteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EditNoteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val NOTE_TITLE = "noteTitle"
+        const val NOTE_BODY = "noteBody"
     }
 }
--- app/src/main/java/com/example/goldennotesdroid/adapter/NotesAdapter.kt
+++ app/src/main/java/com/example/goldennotesdroid/adapter/NotesAdapter.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.goldennotesdroid.NotesFragmentDirections
 import com.example.goldennotesdroid.R
 import com.example.goldennotesdroid.model.Note
+import com.google.android.material.card.MaterialCardView
 
 class NotesAdapter(
     private val context: Context,
     private val dataset: List<Note>
 ) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
-    class NotesViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class NotesViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val noteContainer: MaterialCardView = view.findViewById(R.id.note_container)
         val titleTextView: TextView = view.findViewById(R.id.notes_item_title)
         val bodyTextView: TextView = view.findViewById(R.id.notes_item_body)
     }
@@ -26,8 +30,14 @@ class NotesAdapter(
 
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
         val item = dataset[position]
-        holder.titleTextView.text = context.resources.getString(item.titleResourceId)
-        holder.bodyTextView.text = context.resources.getString(item.bodyResourceId)
+        val titleText = context.resources.getString(item.titleResourceId)
+        val bodyText = context.resources.getString(item.bodyResourceId)
+        holder.titleTextView.text = titleText
+        holder.bodyTextView.text = bodyText
+        holder.noteContainer.setOnClickListener{
+            val action = NotesFragmentDirections.actionNotesFragmentToEditNoteFragment(titleText, bodyText)
+            holder.view.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount() = dataset.size
